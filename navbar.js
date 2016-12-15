75b5a4c20a76d408ea8db89eb6238a7f42f92399
S110846 <S110846@28L-F407-207733.cusd80.lz>
2016-12-15 10:23:03 -0700

    Sitemap added. Dropdown Bar added.

---
+++ navbar.js
@@ -0,0 +1,52 @@
+function buildMap(id) {
+    var sitemap = document.getElementById(id);
+    var ul = document.createElement("ul");
+    ul.className = "Nav";
+    sitemap.appendChild(ul);
+
+    var index = new Array("Home", "index.html",
+            "Biography", "bio.html",
+            "Breakout", "breakout.html",
+            "popup", "popup.html",
+            "Sitemap", "sitemap.html"
+        );
+    for (var i = 0; i < 10; i = i + 2) {
+        var li = document.createElement("li");
+        li.className = "Nav";
+        ul.appendChild(li);
+        var a = document.createElement("a");
+        a.textContent = index[i];
+        a.href = index[i + 1];
+        li.appendChild(a);
+    }
+
+    var dropdown = new Array(
+            "ToDoList", "todolist.html",
+            "TicTacToe", "tictactoe.html",
+            "ChalkBoard", "Chalkboard.html",
+            "Multiplication Table", "MultiplicationTable.html",
+            "Slider", "Mult.2Table.html"
+            );
+
+    var li = document.createElement("li");
+    li.className = "dropdown";
+    ul.appendChild(li);
+    var a = document.createElement("a");
+    a.textContent = "Tools";
+    a.href = "#proj";
+    a.className = "dropbtn";
+    li.appendChild(a);
+    var div = document.createElement("div");
+    div.className = "dropdown-content";
+    li.appendChild(div);
+    for (var i = 0; i < 10; i = i + 2) {
+        var li = document.createElement("li");
+        div.appendChild(li);
+        var a = document.createElement("a");
+        a.textContent = dropdown[i];
+        a.href = dropdown[i + 1];
+        div.appendChild(a);
+    }
+}
+
+
